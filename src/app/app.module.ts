6c3d515ec8ebd731db2cdda3eec8e575f79328ca
Mohadese <amanzadehmohadese@gmail.com>
2023-10-02 18:47:57 +0330

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { HeaderComponent } from './header/header.component';
+import { PerformanceComponent } from './performance/performance.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { MenuBurgerComponent } from './menu-burger/menu-burger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    PageTitleComponent,
+    HeaderComponent,
+    PerformanceComponent,
+    SearchBoxComponent,
+    MenuBurgerComponent
   ],
   imports: [
     BrowserModule,
